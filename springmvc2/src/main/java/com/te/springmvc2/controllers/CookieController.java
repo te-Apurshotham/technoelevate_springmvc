6b2eb96c32fe2be4a6fff3edc4785499a9cd547b
Purshotham041 <81165872+Purshotham041@users.noreply.github.com>
2021-04-21 11:09:06 +0530

    Update CookieController.java

--- springmvc2/src/main/java/com/te/springmvc2/controllers/CookieController.java
+++ springmvc2/src/main/java/com/te/springmvc2/controllers/CookieController.java
@@ -4,34 +4,35 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class CookieController {
-	
+
 	@GetMapping("/cookiesPage")
 	public String getCookies() {
-		return "cookiesPage";
-		
+		return "cookie";
+
 	}
-	
-	@GetMapping("/cookiesPage")
-	public String name(HttpServletResponse respone,ModelMap map) {
-		Cookie cookie = new Cookie("Empname", "Sai");
-		respone.addCookie(cookie);
-		map.addAttribute("msg", "created cookies");
-		return "cookiesPage";
-		
+
+	@GetMapping("/showCookie")
+	public String name(ModelMap map, @CookieValue(name = "EmpName") String name) {
+
+		map.addAttribute("emp", name);
+		return "cookie";
+
 	}
-	
-	@GetMapping()
-	public String showCookies(@CookieValue(name = "EmpName") String name, ModelMap map) {
-		return name;
-		
+
+	@GetMapping("/createcookie")
+	public String createCookies(HttpServletResponse response, ModelMap map) {
+		Cookie cookie = new Cookie("EmpName", "Puru");
+		response.addCookie(cookie);
+		map.addAttribute("msg", "cookie created sucessfully");
+		return "cookie";
 	}
-	
-	
 
 }
